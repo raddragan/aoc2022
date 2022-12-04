07e4e2e762871ae0d91b4ba6c9e3812b11d87a73
Dragan Radovic <dragan.radovic@devfacto.com>
2022-12-04 08:45:21 -0500

    +dec03

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
+def intersection(lst1, lst2):
+    return unique([value for value in lst1 if value in lst2])
+
+def arrIntersection(lst:list):
+    return unique(intersection(intersection(lst[0], lst[1]),lst[2]))
+
 def readInput(day, test):
     fileName = f'input/{day}{".t" if test else ""}'
     lines = open(fileName).read().split('\n')
     return lines
+
+def unique(lst):
+    return (list(set(lst)))
