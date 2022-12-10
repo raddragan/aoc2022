956c5709c64e11c719f396fc8bafc0760e67ec18
Dragan Radovic <dragan.radovic@devfacto.com>
2022-12-10 10:26:10 -0500

    ~ dec10 cleanup

--- dec10.py
+++ dec10.py
@@ -2,7 +2,10 @@ import utils
 import re
 
 def run():
-    adds = [[0,1]]
+    adds = [[-1,1]]
+    def getSprite (at: int):
+        return sum([a[1] for a in filter(lambda a: a[0] < at, adds)])
+
     cycle = 0
     for add in [re.match('addx ([-\d]*)', line) for line in utils.readInput('10', False)]:
         if not add:
@@ -12,19 +15,18 @@ def run():
             adds.append([cycle, int(add.group(1))])
     
     total = 0
-    for snap in [20,60,100,140,180,220]:
-        value = sum([a[1] for a in filter(lambda a: a[0] < snap, adds)])
-        total+=value*snap
-        print(f'{snap} {value} * {snap} = {value*snap}')
+    for cycle in [20,60,100,140,180,220]:
+        value = getSprite(cycle)
+        total += value*cycle
+        print(f'{cycle} {value} * {cycle} = {value*cycle}')
     print(f'total: {total}')
     print('')
 
-    spriteAt = 1
     pixels = []
     for cycle in range (240):
-        spriteAt = sum([a[1] for a in filter(lambda a: a[0] <= cycle, adds)])
+        spriteAt = getSprite(cycle+1)
         position = cycle % 40
-        pixel = '#' if(abs(spriteAt-position)<2) else '.'
+        pixel = '#' if(abs(spriteAt-position) < 2) else '.'
         pixels.append(pixel)
     for y in range(6):
         pixelsInRow = pixels[y*40:(y+1)*40]
